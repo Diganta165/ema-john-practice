f4c7f021e58832ba381df438cfb9cc36e7e72d42
Diganta165 <mdsrdiganta@gmail.com>
2021-09-24 13:44:41 +0600

    Displayed Products & added element length and total to cart

---
+++ src/components/Product/Product.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    console.log(props);
+    const {name, img, seller, price, stock} = props.product;
+    return (
+        <div className="product">
+            <img src={img} />
+            <div>
+            <h4 className="product-name">{name}</h4>
+            <p><small>by: {seller}</small></p>
+            <p>Price: {price}</p>
+            <p><small>only {stock} left in stock - order soon</small></p>
+            <button onClick={()=>props.handleAddToCart(props.product)} className="btn-regular">Add to cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
