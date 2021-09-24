74d450930c14660f899650444094b7d50a97f67d
Diganta165 <mdsrdiganta@gmail.com>
2021-09-24 14:16:08 +0600

    added garnd total, tax & shipping cost to cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 
 const Product = (props) => {
@@ -12,7 +14,7 @@ const Product = (props) => {
             <p><small>by: {seller}</small></p>
             <p>Price: {price}</p>
             <p><small>only {stock} left in stock - order soon</small></p>
-            <button onClick={()=>props.handleAddToCart(props.product)} className="btn-regular">Add to cart</button>
+            <button onClick={()=>props.handleAddToCart(props.product)} className="btn-regular"><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
             </div>
         </div>
     );
