3df83cc8b73215161d40caa1566b56f5b47106ff
Diganta165 <mdsrdiganta@gmail.com>
2021-10-15 23:28:54 +0600

    added login and register component with route

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCZ3PEK5e21a4mw9T6-nRirGZhwPitEQZU",
+    authDomain: "ema-john-simple-practice-5672b.firebaseapp.com",
+    projectId: "ema-john-simple-practice-5672b",
+    storageBucket: "ema-john-simple-practice-5672b.appspot.com",
+    messagingSenderId: "252144542467",
+    appId: "1:252144542467:web:7a05e2ca693cd1a96cbf36"
+  };
+
+  export default firebaseConfig;
